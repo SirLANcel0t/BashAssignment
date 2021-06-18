fb4d966e4210176fadcb66ca9905290d6a19ab10
Xodek <70526506+Xodek@users.noreply.github.com>
2021-06-18 06:50:42 -0400

    Update RmMenu_1.sh
    sappig 2.0
    RM menu voor zover ik weet compleet. Gevonden bugs gefixt. Redelijk goed getest.

--- RmMenu_1.sh
+++ RmMenu_1.sh
@@ -14,32 +14,42 @@
 undelete="false"
 haspassword="false"
 recursive="false"
+filename=""
+password=""
 
 while [ $# -gt 0 ]
 do
-    if [ $1 == "-u" ] && [ $haspassword == "false" ]
+    if [[ $1 == "-u" && $haspassword == "false" ]]
     then
         undelete="true"
 
-    elif [ $1 == "-p" ] && [ $undelete == "false" ]
+    elif [[ $1 == "-p" && $undelete == "false" ]]
     then
         haspassword="true"
-        setPassword="true"
+        setpassword="true"
     
     elif [ $1 == "-r" ]
     then
         recursive="true"
     
-    elif [ $1 != "-p" ] && [ $1 != "-u" ] && [ $1 != "-r" ] && [ $setPassword == "true" ]
+    elif [[ $1 != "-p" && $1 != "-u" && $1 != "-r" && $setpassword == "true" ]]
     then
-        setPassword="false"
+        setpassword="false"
         password=$1
-    
+        
     elif [[ $1 == "-p" && $undelete == "true" ]] || [[ $1 == "-u" && $haspassword == "true" ]]
     then
-        echo "Sorry, dit is geen geldige input!"
+        echo "rm+: cannot use -u and -p at the same time"
+        
+    elif [[ $undelete == "false" && $recursive == "false" && -d ~/$1 ]]
+    then
+        echo "rm+: cannot remove a directory without -r"
+        
+    elif ! [[ -e $1 || -e ~/$1 || $1 == "-u" || $1 == "-p" || $1 == "-r" ]]
+    then
+        echo "rm+: this file or directory does not exist"
     else
-        fileName=$1
+        filename=$1
     fi
 shift
 done
